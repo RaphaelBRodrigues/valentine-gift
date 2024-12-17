98545b3a0f2334542d94e13f29999f206cf78e66
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2024-12-17 18:40:54 -0300

    updating timer

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react"
 
 const initialDate = new Date("2023-12-14 12:00:00");
 const currentDate = new Date();
+currentDate.setHours(12, 0, 0);
 
 const initialSeconds = Math.round(Math.abs((currentDate.getTime() - initialDate.getTime()) / 1000));
 const initialMinutes = Math.floor(initialSeconds / 60);
@@ -12,13 +13,15 @@ export const useTimer = () => {
   const [seconds, setSeconds] = useState(initialSeconds);
   const [minutes, setMinutes] = useState(initialMinutes);
   const [hour, setHours] = useState(initialHours);
-  const [days, setDays] = useState(initialDays);
+  const [days, setDays] = useState(initialDays - 1);
   const [weeks, setWeeks] = useState(0);
   const [months, setMonths] = useState(0);
   const [years, setYears] = useState(0);
 
   const updateTimer = useCallback(() => {
     const currentDate = new Date();
+    currentDate.setHours(12, 0, 0);
+
     const totalSeconds = Math.round(Math.abs((currentDate.getTime() - initialDate.getTime()) / 1000));
 
     const totalMinutes = Math.floor(totalSeconds / 60);
